0e405280871d81f6986bf85f6b7cacffe42ba9d2
Proxypepe <37917466+Proxypepe@users.noreply.github.com>
2021-02-21 08:41:53 +0300

    Iter added

--- Double_linked_list.py
+++ Double_linked_list.py
@@ -2,30 +2,34 @@ from Linked_list import Linked_list
 
 class Node:
     def __init__(self, value: object) -> object:
-        self._next = None 
-        self._tail = Node
+        self._next = None
+        self._tail = None
         self._value = value
 
-class double_list(Linked_list):
-    
+class Double_list(Linked_list):
+
     def __init__(self):
-        self.__head = None  
-        self.__tail = None 
+        self.__head = None
+        self.__tail = None
+        self.__iter = None
+        self.__len  = 0
+        self.__currect_position = 0
 
     def push_back(self, value :object):
         if self.__head == None and self.__tail == None:
             self.__head = self.__tail = Node(value)
+            self.__len += 1
         else:
             _tmp = Node(value)
             _tmp.__tail = self.__tail
             self.__tail._next = _tmp
             self.__tail = _tmp
-          
-    
+            self.__len += 1
+
     def pop_back() -> None:
-        #TODO    
-        pass   
-    
+        #TODO
+        pass
+
     def push_front(value: object) -> None:
         #TODO
         pass
@@ -33,19 +37,24 @@ class double_list(Linked_list):
     def pop_front() -> None:
         #TODO
         pass
-    
+
     def __len__(self):
         #TODO
-        pass
-    
+        return self.__len
+
     def __str__(self):
         #TODO
         pass
 
-    def print_list(self) -> None:
-        q = self.__head
-        #print(type(q))
-        while(q is not None):
-            print(q._value)
-            q = q._next
-        
+    def __iter__(self):
+        self.__iter = self.__head
+        self.__currect_position = 0
+        return self
+
+    def __next__(self):
+        self.__currect_position += 1
+        if self.__currect_position > self.__len:
+            raise StopIteration
+        now = self.__iter._value
+        self.__iter = self.__iter._next
+        return now
---
+++ test.py
@@ -0,0 +1,15 @@
+from Double_linked_list import Double_list
+
+def main():
+
+    myList = Double_list()
+
+    myList.push_back(12)
+    myList.push_back(13)
+    myList.push_back(14)
+
+
+
+
+if __name__ == '__main__':
+    main()

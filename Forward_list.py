dc7245b3ea8033c72197e246c62f943b06d8658f
Proxypepe <37917466+Proxypepe@users.noreply.github.com>
2021-03-01 18:48:51 +0300

    Minimal imp

---
+++ Forward_list.py
@@ -0,0 +1,130 @@
+import Linked_list
+
+
+class Node:
+    def __init__(self, value: object) -> object:
+        self._next = None
+        self._value = value
+
+class ForwardList(Linked_list.LinkedListAbstract):
+
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__iter = None
+        self.__len = 0
+        self.__current_position = 0
+
+    def push_back(self, value: object) -> None:
+        if self.is_empty():
+            self.__head = self.__tail = Node(value)
+        else:
+            self.__tail._next = Node(value)
+            self.__tail = self.__tail._next
+        self.__len += 1
+
+    def pop_back(self) -> None:
+        if not self.is_empty():
+            _tmp = self.__head
+            if self.__len == 1:
+                del self.__head._next
+                self.__head._next = None
+                self.__tail = self.__head
+            else:
+                while _tmp._next._next is not None:
+                    _tmp = _tmp._next
+                del _tmp._next
+                _tmp._next = None
+                self.__tail = _tmp
+            self.__len -= 1
+
+    def push_front(self, value: object) -> None:
+        if self.is_empty():
+            self.__head = self.__tail = Node(value)
+        else:
+            if self.__len == 1:
+                _tmp = Node(value)
+                _tmp._next = self.__head
+                self.__head = _tmp
+                self.__tail = self.__head._next
+            else:
+                _tmp = Node(value)
+                _tmp._next = self.__head
+                self.__head = _tmp
+
+        self.__len += 1
+
+    def pop_front(self) -> None:
+        if not self.is_empty():
+            _tmp = self.__head
+            self.__head = self.__head._next
+            del _tmp
+
+            self.__len -= 1
+
+    def insert_before(self, pos: int, value: object) -> None:
+        if pos > self.__len or pos < 1:
+            raise IndexError
+        else:
+            if self.is_empty():
+                self.__head = self.__tail = Node(value)
+            elif pos == 1:
+                self.push_front(value)
+            elif pos == self.__len:
+                self.push_back(value)
+            else:
+                _q = self.__head
+                _tmp = Node(value)
+                _counter = 0
+                for i in range(pos - 2):
+                    _q = _q._next
+                _tmp._next = _q._next
+                _q._next = _tmp
+                self.__len += 1
+
+    def insert_after(self, pos: int, value: object) -> None:
+        if pos > self.__len or pos < 1:
+            raise IndexError
+        else:
+            if self.is_empty():
+                self.__head = self.__tail = Node(value)
+            elif pos == 1:
+                self.push_front(value)
+            elif pos == self.__len:
+                self.push_back(value)
+            else:
+                _q = self.__head
+                _tmp = Node(value)
+                _counter = 0
+                for i in range(pos - 1):
+                    _q = _q._next
+                _tmp._next = _q._next
+                _q._next = _tmp
+                self.__len += 1
+
+    def erase(self):
+        pass
+
+    def remove(self, pos: int):
+        pass
+
+    def is_empty(self) -> bool:
+        if self.__head is None:
+            return True
+        return False
+
+    def __len__(self):
+        return self.__len
+
+    def __iter__(self):
+        self.__iter = self.__head
+        self.__current_position = 0
+        return self
+
+    def __next__(self):
+        self.__current_position += 1
+        if self.__current_position > self.__len:
+            raise StopIteration
+        now = self.__iter._value
+        self.__iter = self.__iter._next
+        return now
